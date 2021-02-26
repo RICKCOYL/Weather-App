64c4006d225c098d00137d855b4fc8b473a36ebe
RICKCOYL <rickoburu@gmail.com>
2021-02-26 19:07:21 +0300

    add UI data

--- src/components/app.js
+++ src/components/app.js
@@ -2,11 +2,19 @@ function populateDom() {
     const city = document.querySelector('#city').value;
     const apiKey = 'c5b93e5f844aed131edab3bb66a3d62d';
     const wLink = (`http://api.openweathermap.org/data/2.5/weather?id=524901&appid=${apiKey}`);
-    //console.log(responseData.weather);
+
     fetch(wLink).then(res => {
         return res.json();
     }).then(function (res) {
-        console.log(res);
+        console.log(res.weather[0]);
+        document.querySelector('#location').textContent = `${res.name}, ${res.sys.country}`;
+        document.querySelector('#description').textContent = `${res.weather[0].description}`;
+        document.querySelector('#icon').setAttribute('src', `http://openweathermap.org/img/wn/${res.weather[0].icon}@2x.png`);
+    }).catch(() => {
+        const errorMessage = document.querySelector('#error');
+        errorMessage.setAttribute('class', 'alert alert-warning');
+        errorMessage.setAttribute('role', 'alert');
+        errorMessage.textContent = 'PLEASE SEARCH FOR A VALID CITY';
     });
 
 };
