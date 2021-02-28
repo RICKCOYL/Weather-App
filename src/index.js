2ae5431f81c0c3b0ed665886840da6110d0aacf0
RICKCOYL <rickoburu@gmail.com>
2021-02-28 11:41:35 +0300

    fix linter errors

--- src/index.js
+++ src/index.js
@@ -10,23 +10,23 @@ const ui = new UI();
 
 
 function getWeather() {
-    weather.getWeather()
-        .then(results => {
-            ui.paint(results);
-        })
-        .catch(err => console.log(err));
+  weather.getWeather()
+    .then(results => {
+      ui.paint(results);
+    })
+    .catch(err => err);
 }
 
 document.addEventListener('DOMContentLoaded', getWeather);
 
 document.getElementById('w-change-btn').addEventListener('click', () => {
-    const city = document.getElementById('city').value;
+  const city = document.getElementById('city').value;
 
-    weather.changeLocation(city);
+  weather.changeLocation(city);
 
-    storage.setLocationData(city);
+  storage.setLocationData(city);
 
-    getWeather();
+  getWeather();
 
-    $('#locModal').modal('hide');
+  $('#locModal').modal('hide');
 });
