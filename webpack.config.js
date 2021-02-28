c84661e57852cad9d46f7be7d145988c8605fdb5
RICKCOYL <rickoburu@gmail.com>
2021-02-28 12:42:10 +0300

    fix linter errors

--- webpack.config.js
+++ webpack.config.js
@@ -9,10 +9,6 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
         test: /\.worker\.js$/,
         use: { loader: 'worker-loader' },
       },
