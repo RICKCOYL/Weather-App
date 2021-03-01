6fef5bade0f88816917f50a439e87527e3894834
RICKCOYL <rickoburu@gmail.com>
2021-03-01 12:26:34 +0300

    change webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,29 @@
-const path = require('path');
+import { resolve } from 'path';
 
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.worker\.js$/,
-        use: { loader: 'worker-loader' },
-      },
-      {
-        test: /\.m?js$/,
-        exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
-          },
+export const entry = './src/index.js';
+export const output = {
+  filename: 'main.js',
+  path: resolve(__dirname, 'dist'),
+};
+export const module = {
+  rules: [
+    {
+      test: /\.css$/i,
+      use: ['style-loader', 'css-loader'],
+    },
+    {
+      test: /\.worker\.js$/,
+      use: { loader: 'worker-loader' },
+    },
+    {
+      test: /\.m?js$/,
+      exclude: /(node_modules|bower_components)/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env'],
         },
       },
-    ],
-  },
+    },
+  ],
 };
