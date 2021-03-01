01bfc7faeac0bc847eb8c047e9de379784d5543b
RICKCOYL <rickoburu@gmail.com>
2021-03-01 19:32:46 +0300

    add change to api

--- src/components/weather.js
+++ src/components/weather.js
@@ -5,7 +5,7 @@ export default class Weather {
   }
 
   async getWeather() {
-    const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${this.city},${this.country}id=524901&appid=c5b93e5f844aed131edab3bb66a3d62d&units=metric.json`);
+    const response = await fetch(`https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/weather?q=${this.city},${this.country}id=524901&appid=c5b93e5f844aed131edab3bb66a3d62d&units=metric.json`);
 
     const responseData = await response.json();
 
