fd09de5f8cbf3a7c8b51b9f13be43d43f1170f51
RICKCOYL <rickoburu@gmail.com>
2021-03-01 12:35:38 +0300

    change json file

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,31 @@
-import { resolve } from 'path';
+const path = require('path');
 
-export const entry = './src/index.js';
-export const output = {
-  filename: 'main.js',
-  path: resolve(__dirname, 'dist'),
-};
-export const module = {
-  rules: [
-    {
-      test: /\.css$/i,
-      use: ['style-loader', 'css-loader'],
-    },
-    {
-      test: /\.worker\.js$/,
-      use: { loader: 'worker-loader' },
-    },
-    {
-      test: /\.m?js$/,
-      exclude: /(node_modules|bower_components)/,
-      use: {
-        loader: 'babel-loader',
-        options: {
-          presets: ['@babel/preset-env'],
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.worker\.js$/,
+        use: { loader: 'worker-loader' },
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
         },
       },
-    },
-  ],
+    ],
+  },
 };
